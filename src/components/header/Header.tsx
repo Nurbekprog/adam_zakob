16db8da11e2d89b8ac1529e84a27e4728a5b6976
Nurbekprog <nurprogdata@gamil.com>
2025-10-07 09:42:28 +0500

    first commit

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,35 @@
+import Logo from "../../assets/images/logo.png";
+import Menu_btn from "../../assets/images/menu_btn.png";
+import "./Header.scss";
+const Header = () => {
+  return (
+    <div className="container pt-65">
+      <ul className="nav flex place-content-between items-center">
+        <li className="flex place-content-between gap-[100px] items-center">
+          <img src={Logo} alt="Logo" />
+          <li className="flex gap-15 d-none d-lg-block">
+            <a href="/about" className="links">
+              About
+            </a>
+            <a href="/services" className="links">
+              Services
+            </a>
+            <a href="/works" className="links">
+              Works
+            </a>
+            <a href="/blog" className="links">
+              Blog
+            </a>
+          </li>
+        </li>
+        <li>
+          <button className="menu_btn">
+            <img src={Menu_btn} alt="" />
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Header;
